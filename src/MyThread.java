0199b374b6cd2ce031286fbf56352c154c87ca56
Oleg <sosolala404@gmail.com>
2021-07-03 17:51:44 +0300

    constructor edit

--- src/MyThread.java
+++ src/MyThread.java
@@ -3,12 +3,18 @@ import java.util.concurrent.*;
 
 class MyThread extends Thread {
 
+    public MyThread(ThreadGroup group) {}
+
+    /*public MyThread(String superName) {
+        this.superName = superName;
+    }
+*/
     @Override
     public void run() {
         try {
             while(!isInterrupted()) {
                 Thread.sleep(3000);
-                System.out.println("я" + Thread.getCurrentThread.getName() + "Всем привет!");
+                System.out.println("я" + Thread.currentThread() + "Всем привет!");
             }
         } catch (InterruptedException err) {
 
